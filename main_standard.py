1c19849b2a0ac3c68798a6b808483b2849afa17f
Wightslayer <552188@student.inholland.nl>
2021-03-01 11:15:05 +0100

    Cleaning

---
+++ main_standard.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+import argparse
+import datetime
+import numpy as np
+import time
+import torch
+import torch.backends.cudnn as cudnn
+import os
+
+import models  # Need to register the models!
+from timm.models import create_model
+
+import importlib
+
+from trainer import Trainer
+from config import cfg
+from shutil import copyfile
+
+# __all__ = [
+#     'deit_base_patch16_224',              'deit_small_patch16_224',               'deit_tiny_patch16_224',
+#     'deit_base_distilled_patch16_224',    'deit_small_distilled_patch16_224',     'deit_tiny_distilled_patch16_224'
+#     'deit_base_patch16_384',
+#     'deit_base_distilled_patch16_384',
+# ]
+
+
+model_mapping = {
+    'deit_tiny_cnn_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth',
+    'deit_tiny_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth',
+    'deit_tiny_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_tiny_distilled_patch16_224-b40b3cf7.pth',
+    'deit_small_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth',
+    'deit_small_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_small_distilled_patch16_224-649709d9.pth',
+    'deit_base_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth',
+    'deit_base_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_224-df68dfff.pth',
+    'deit_base_patch16_384': 'https://dl.fbaipublicfiles.com/deit/deit_base_patch16_384-8de9b5d1.pth',
+    'deit_base_distilled_patch16_384': 'https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_384-d0272ac0.pth'
+}
+
+
+def make_save_dirs(loaded_cfg):
+    if not os.path.exists(loaded_cfg.SAVE_DIR):
+        os.mkdir(loaded_cfg.SAVE_DIR)
+        os.mkdir(loaded_cfg.PICS_DIR)
+        os.mkdir(loaded_cfg.STATE_DICTS_DIR)
+        os.mkdir(loaded_cfg.CODE_DIR)
+        with open(os.path.join(cfg.SAVE_DIR, '__init__.py'), 'w') as f:  # For dynamic loading of config file
+            pass
+
+    else:
+        print('save directory already exists!')
+
+
+def main(cfg):
+    if cfg.RESUME:
+        module = importlib.import_module(cfg.RESUME_DIR.replace(os.sep, '.') + 'code.config')
+        cfg = module.cfg
+    else:
+        make_save_dirs(cfg)
+        copyfile('config.py', os.path.join(cfg.CODE_DIR, 'config.py'))
+        copyfile('trainer.py', os.path.join(cfg.CODE_DIR, 'trainer.py'))
+        copyfile('models.py', os.path.join(cfg.CODE_DIR, 'models.py'))
+        copyfile(os.path.join('datasets', cfg.DATASET, 'settings.py'),
+                 os.path.join(cfg.CODE_DIR, 'settings.py'))
+        copyfile(os.path.join('datasets', cfg.DATASET, 'loading_data.py'),
+                 os.path.join(cfg.CODE_DIR, 'loading_data.py'))
+        copyfile(os.path.join('datasets', cfg.DATASET, cfg.DATASET + '.py'),
+                 os.path.join(cfg.CODE_DIR, cfg.DATASET + '.py'))
+
+    # fix the seed for reproducibility
+    torch.manual_seed(cfg.SEED)
+    np.random.seed(cfg.SEED)
+    # random.seed(seed)
+
+    cudnn.benchmark = True
+
+    print(f"Creating model: {cfg.MODEL}")
+
+    model = create_model(
+        cfg.MODEL,
+        init_path=model_mapping[cfg.MODEL],
+        num_classes=1000,  # Not yet used anyway. Must match pretrained model!
+        drop_rate=0.,
+        drop_path_rate=0.1,  # TODO: What does this do?
+        drop_block_rate=None,
+    )
+
+    model.cuda()
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of params:', n_parameters)
+
+    dataloader = importlib.import_module(f'datasets.{cfg.DATASET}.loading_data').loading_data
+    cfg_data = importlib.import_module(f'datasets.{cfg.DATASET}.settings').cfg_data
+
+    trainer = Trainer(model, dataloader, cfg, cfg_data)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main(cfg)
---
+++ trainer_standard.py
@@ -0,0 +1,236 @@
+import os
+import time
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from datasets.dataset_utils import img_equal_unsplit
+import matplotlib.pyplot as plt
+from matplotlib import cm as CM
+
+
+class Trainer:
+    def __init__(self, model, loading_data, cfg, cfg_data):
+        self.model = model
+        self.cfg = cfg
+        self.cfg_data = cfg_data
+
+        self.train_loader, self.test_loader, self.restore_transform = loading_data(self.model.crop_size)
+        self.train_samples = len(self.train_loader.dataset)
+        self.test_samples = len(self.test_loader.dataset)
+        self.eval_save_example_every = self.test_samples // self.cfg.SAVE_NUM_EVAL_EXAMPLES
+
+        self.criterion = torch.nn.MSELoss()
+        self.optim = torch.optim.Adam(model.parameters(), lr=cfg.LR, weight_decay=cfg.WEIGHT_DECAY)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, step_size=1, gamma=cfg.LR_GAMMA)
+
+        self.epoch = 0
+        self.best_mae = 10 ** 10  # just something high
+        self.best_epoch = -1
+
+        self.writer = SummaryWriter(cfg.SAVE_DIR)
+
+        if cfg.RESUME:
+            self.load_state(cfg.RESUME_PATH)
+            print(f'Resuming from epoch {self.epoch}')
+        # else:
+        #     self.save_eval_pics()
+        #     self.writer.add_scalar('lr', self.scheduler.get_last_lr()[0], self.epoch)
+
+    def train(self):
+        # MAE = self.evaluate_model()
+        # print(f'Initial MAE: {MAE:.3f}')
+
+        self.model.train()
+        while self.epoch < self.cfg.MAX_EPOCH:
+            self.epoch += 1
+
+            epoch_start_time = time.time()
+            den_losses, count_losses, total_losses, errors, last_out_den, last_gts = self.run_epoch()
+            epoch_time = time.time() - epoch_start_time
+
+            MAE = torch.mean(torch.stack(errors)) / (self.cfg_data.TRAIN_BS * self.cfg_data.LABEL_FACTOR)
+            avg_den_loss = np.mean(den_losses)
+            avg_count_loss = np.mean(count_losses)
+            avg_loss = np.mean(total_losses)
+            pred_cnt = last_out_den[0].detach().cpu().sum() / self.cfg_data.LABEL_FACTOR
+            gt_cnt = last_gts[0].cpu().sum() / self.cfg_data.LABEL_FACTOR
+            print(f'ep {self.epoch}: Average loss={avg_loss:.3f}, Patch MAE={MAE:.3f}.'
+                  f'  Example: pred={pred_cnt:.3f}, gt={gt_cnt:.3f}. Train time: {epoch_time:.3f}')
+
+            self.writer.add_scalar('Loss/train_den', avg_den_loss, self.epoch)
+            self.writer.add_scalar('Loss/train_count', avg_count_loss, self.epoch)
+            self.writer.add_scalar('Loss/train_combined', avg_loss, self.epoch)
+            self.writer.add_scalar('MAE/train', MAE, self.epoch)
+
+            if self.epoch % self.cfg.EVAL_EVERY == 0:
+                eval_start_time = time.time()
+                eval_MAE = self.evaluate_model()
+                eval_time = time.time() - eval_start_time
+
+                if eval_MAE < self.best_mae:
+                    self.best_mae = eval_MAE
+                    self.best_epoch = self.epoch
+                    print_fancy_new_best_MAE()
+                    self.save_state(f'new_best_MAE_{eval_MAE:.3f}')
+                elif self.epoch % self.cfg.SAVE_EVERY == 0:
+                    self.save_state(f'MAE_{eval_MAE:.3f}')
+
+                print(f'MAE: {eval_MAE:.3f}, best MAE: {self.best_mae:.3f} at ep({self.best_epoch}).'
+                      f' eval time: {eval_time:.3f}')
+
+                self.writer.add_scalar('MAE/test', eval_MAE, self.epoch)
+
+            if self.epoch in self.cfg.LR_STEP_EPOCHS:
+                self.scheduler.step()
+                print(f'Learning rate adjusted to {self.scheduler.get_last_lr()[0]} at epoch {self.epoch}.')
+                self.writer.add_scalar('lr', self.scheduler.get_last_lr()[0], self.epoch)
+
+    def run_epoch(self):
+        den_losses = []
+        count_losses = []
+        total_losses = []
+        errors = []
+
+        out_den = None  # SILENCE WENCH!
+        den_gts = None  # silences the 'might not be defined' warning below the for loop.
+
+        for idx, (images, gts) in enumerate(self.train_loader):
+            images = images.cuda()
+            den_gts = gts.squeeze().cuda()
+            count_gts = den_gts.sum(dim=(1, 2)).unsqueeze(-1)
+
+            self.optim.zero_grad()
+            out_den, out_count = self.model(images)
+            out_den = out_den.squeeze()
+
+            den_loss = self.criterion(out_den, den_gts)
+            count_loss = self.criterion(out_count, count_gts)
+
+            total_loss = den_loss + self.cfg.COUNT_LOSS_FACTOR * count_loss
+            total_loss.backward()
+            self.optim.step()
+
+            den_losses.append(den_loss.cpu().item())
+            count_losses.append(den_loss.cpu().item())
+            total_losses.append((total_loss.cpu().item()))
+            errors.append(torch.abs(torch.sum(out_den - den_gts, dim=(1, 2))).sum())
+
+        # Also return the last predicted densities and corresponding gts. This allows for informative prints
+        return den_losses, count_losses, total_losses, errors, out_den, den_gts
+
+    def evaluate_model(self):
+
+        plt.cla()  # Clear plot for new ones
+        self.model.eval()
+        with torch.no_grad():
+            errors = []
+
+            abs_patch_errors = torch.zeros(self.model.crop_size, self.model.crop_size)
+            summed_patch_errors = torch.zeros(self.model.n_patches, self.model.n_patches)
+
+            for idx, (img, img_patches, gt_patches) in enumerate(self.test_loader):
+                img_patches = img_patches.squeeze().cuda()
+                gt_patches = gt_patches.squeeze().unsqueeze(1)  # Remove batch dim, insert channel dim
+                img = img.squeeze()  # Remove batch dimension
+                _, img_h, img_w = img.shape
+
+                pred_den, pred_count = self.model(img_patches)
+                pred_den = pred_den.cpu()
+
+                gt = img_equal_unsplit(gt_patches, self.cfg_data.OVERLAP, self.cfg_data.IGNORE_BUFFER, img_h, img_w, 1)
+                den = img_equal_unsplit(pred_den, self.cfg_data.OVERLAP, self.cfg_data.IGNORE_BUFFER, img_h, img_w, 1)
+                den = den.squeeze()  # Remove channel dim
+
+                pred_cnt = den.sum() / self.cfg_data.LABEL_FACTOR
+                gt_cnt = gt.sum() / self.cfg_data.LABEL_FACTOR
+                errors.append(torch.abs(pred_cnt - gt_cnt))
+
+                if idx % self.eval_save_example_every == 0:
+                    plt.imshow(den, cmap=CM.jet)
+                    save_path = os.path.join(self.cfg.PICS_DIR, f'pred_{idx}_ep_{self.epoch}.jpg')
+                    plt.title(f'Predicted count: {pred_cnt:.3f} (GT: {gt_cnt:.3f})')
+                    plt.savefig(save_path)
+
+                abs_patch_errors += torch.sum(torch.abs(gt_patches.squeeze() - pred_den.squeeze()), dim=0)
+            for i in range(14):
+                for j in range(14):
+                    lf = self.cfg_data.LABEL_FACTOR  # So next line fits on 1 line
+                    summed_patch_errors[i, j] = abs_patch_errors[i * 16:(i + 1) * 16, j * 16:(j + 1) * 16].sum() / lf
+
+            MAE = torch.mean(torch.stack(errors))
+
+        plt.cla()
+        plt.imshow(abs_patch_errors)
+        save_path = os.path.join(self.cfg.PICS_DIR, f'errors_ep_{self.epoch}.jpg')
+        plt.savefig(save_path)
+        plt.imshow(summed_patch_errors)
+        save_path = os.path.join(self.cfg.PICS_DIR, f'summed_patch_ep_{self.epoch}.jpg')
+        plt.savefig(save_path)
+
+        return MAE
+
+    def save_eval_pics(self):
+        plt.cla()
+        for idx, (img, img_patches, gt_patches) in enumerate(self.test_loader):
+            gt_patches = gt_patches.squeeze().unsqueeze(1)  # Remove batch dim, insert channel dim
+            img = img.squeeze()
+
+            _, img_h, img_w = img.shape
+
+            gt = img_equal_unsplit(gt_patches, self.cfg_data.OVERLAP, self.cfg_data.IGNORE_BUFFER, img_h, img_w, 1)
+            gt = gt.squeeze()  # Remove channel dim
+
+            if idx % self.eval_save_example_every == 0:
+                img = self.restore_transform(img)
+                gt_count = gt.sum() / self.cfg_data.LABEL_FACTOR
+                gt_count = torch.round(gt_count)
+
+                plt.imshow(img)
+                save_path = os.path.join(self.cfg.PICS_DIR, f'img_{idx}.jpg')
+                plt.title(f'GT count: {gt_count:.3f}')
+                plt.savefig(save_path)
+
+                plt.imshow(gt, cmap=CM.jet)
+                save_path = os.path.join(self.cfg.PICS_DIR, f'gt_{idx}.jpg')
+                plt.title(f'GT count: {gt_count:.3f}')
+                plt.savefig(save_path)
+
+    def save_state(self, name_extra=''):
+        if name_extra:
+            save_name = f'{self.cfg.STATE_DICTS_DIR}/save_state_ep_{self.epoch}_{name_extra}.pth'
+        else:
+            save_name = f'{self.cfg.STATE_DICTS_DIR}/save_state_ep_{self.epoch}.pth'
+
+        save_sate = {
+            'epoch': self.epoch,
+            'best_epoch': self.best_epoch,
+            'best_mae': self.best_mae,
+            'net': self.model.state_dict(),
+            'optim': self.optim.state_dict(),
+            'scheduler': self.scheduler.state_dict(),
+            'save_dir_path': self.cfg.SAVE_DIR,
+        }
+
+        torch.save(save_sate, save_name)
+
+    def load_state(self, state_path):
+        resume_state = torch.load(state_path)
+        self.epoch = resume_state['epoch']
+        self.best_epoch = resume_state['best_epoch']
+        self.best_mae = resume_state['best_mae']
+
+        self.model.load_state_dict(resume_state['net'])
+        self.optim.load_state_dict(resume_state['optim'])
+        self.scheduler.load_state_dict(resume_state['scheduler'])
+
+
+def print_fancy_new_best_MAE():
+    """ For that extra bit of dopamine rush when you get a new high-score"""
+
+    new_best = '#' + '=' * 20 + '<' * 3 + ' NEW BEST MAE ' + '>' * 3 + '=' * 20 + '#'
+    n_chars = len(new_best)
+    bar = '#' + '=' * (n_chars - 2) + '#'
+    print(bar)
+    print(new_best)
+    print(bar)
