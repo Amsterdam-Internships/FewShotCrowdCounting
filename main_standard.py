8fe5df5b07057e19279ed330d1aa9380241cfb02
Wightslayer <552188@student.inholland.nl>
2021-06-19 12:11:06 +0200

    Some comments

--- main_standard.py
+++ main_standard.py
@@ -16,7 +16,7 @@ from shutil import copyfile
 import random
 
 
-# Where to find the pretrained models
+# Where to find the pretrained models.
 model_mapping = {
     'deit_tiny_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth',
     'deit_tiny_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_tiny_distilled_patch16_224-b40b3cf7.pth',
@@ -24,8 +24,6 @@ model_mapping = {
     'deit_small_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_small_distilled_patch16_224-649709d9.pth',
     'deit_base_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth',
     'deit_base_distilled_patch16_224': 'https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_224-df68dfff.pth',
-    'deit_base_patch16_384': 'https://dl.fbaipublicfiles.com/deit/deit_base_patch16_384-8de9b5d1.pth',
-    'deit_base_distilled_patch16_384': 'https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_384-d0272ac0.pth'
 }
 
 
@@ -44,7 +42,15 @@ def make_save_dirs(loaded_cfg):
 
 
 def main(cfg):
-    """ Loads the settings and model, then creates a trainer with which the model is trained."""
+    """
+    Main does the following
+    1) Creates the save directories when starting training. Loads the directory paths when continue straining
+        1.1) (new training only) Make a backup of some important files for archiving and reproducibility purposes.
+    2) Sets seeds for reproducibility
+    3) Makes the model
+    4) Gets the function with which the datloaders can be obtained. Also loads the dataset specific settings.
+    5) Makes the trainer object for training and calls train to train the model (also when fine-tuning)
+    Loads the settings and model, then creates a trainer with which the model is trained."""
 
     if cfg.RESUME:  # Not fully tested yet
         module = importlib.import_module(cfg.RESUME_DIR.replace(os.sep, '.') + 'code.config')
@@ -71,25 +77,25 @@ def main(cfg):
     print(f"Creating model: {cfg.MODEL}")
 
     # Default settings from the original DeiT framework
-    model = create_model(
-        cfg.MODEL,
-        init_path=model_mapping[cfg.MODEL],
+    model = create_model(  # From the timm library. This function created the model specific architecture.
+        cfg.MODEL,  # Which model to use (e.g. DeiT tiny, DeiT small, DeiT base).
+        init_path=model_mapping[cfg.MODEL],  # Where the pretrained weights of ImageNet are saved
         num_classes=1000,  # Not used. But must match pretrained model!
-        drop_rate=0.,
-        drop_path_rate=0.,
-        drop_block_rate=None,
+        drop_rate=0.,  # Dropout
+        drop_path_rate=0.,  # Bamboozled by Facebook. This isn't drop_path_rate, but rather 'drop_connect'
+        drop_block_rate=None,  # Drops our entire Transformer blocks I think? Not used for ViCCT.
     )
 
-    model.cuda()
+    model.cuda()  # CPU training not supported.
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('number of params:', n_parameters)
+    print('number of params:', n_parameters)  # Print the number of trainable parameters of the model
 
-    # Dynamically load the dataloader and its settings as specified in the config file
+    # Dynamically loads the dataloader and its settings as specified in the config file
     dataloader = importlib.import_module(f'datasets.standard.{cfg.DATASET}.loading_data').loading_data
     cfg_data = importlib.import_module(f'datasets.standard.{cfg.DATASET}.settings').cfg_data
 
-    trainer = Trainer(model, dataloader, cfg, cfg_data)  # Make a trainer
+    trainer = Trainer(model, dataloader, cfg, cfg_data)  # Make a trainer object
     trainer.train()  # Train the model
 
 
